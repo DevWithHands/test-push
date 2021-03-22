a163df513024338796418794bde196abf56a7fc1
DevWithHands <mail@doby.pro>
2021-03-22 23:40:57 +0200

    new

--- push.js
+++ push.js
@@ -5,7 +5,7 @@ let vapidKeys = {
     privateKey: 'rDwruI__8N8F_jmK5Dykiz3W5IDqcSQ9323KgXsdMGc'
   }
 
-push.setVapidDetails('mail-to: info@printua.info', vapidKeys.publicKey, vapidKeys.privateKey);
+push.setVapidDetails('mail-to:ninfo@printua.info', vapidKeys.publicKey, vapidKeys.privateKey);
 
 let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cWoZnkTxFP4:APA91bHjCeQ19g9AqAr9GwkJdJtcz1t_xQuodKJybgd9J1lXq7wSxvwTfhak_PtuHQI5o_1TA55_YhdfSFL8FEKlu80qior_FbI41Jgt7n8oSTVcH9Sufje0ob4yFt1JIpfw57Rv1mXw","expirationTime":null,"keys":{"p256dh":"BPfJt1uldLssNsrM7vfymzUHXBw_KI2I8GHt_oX23aUPiCkZ_5yO3R53Yzt78B1aOT2U5r7GUuEvPKkZrDIZJ6U","auth":"Pp2BPubL0IPWsOMliByfjA"}};
 
