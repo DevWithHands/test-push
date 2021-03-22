bb68059408d5808816d9699ee981c8baf792e9f9
DevWithHands <mail@doby.pro>
2021-03-22 23:05:44 +0200

    fix

--- push.js
+++ push.js
@@ -7,6 +7,13 @@ let vapidKeys = {
 
 push.setVapidDetails('mail-to: info@printua.info', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+let sub = {
+  "endpoint":"https://fcm.googleapis.com/fcm/send/cWoZnkTxFP4:APA91bHjCeQ19g9AqAr9GwkJdJtcz1t_xQuodKJybgd9J1lXq7wSxvwTfhak_PtuHQI5o_1TA55_YhdfSFL8FEKlu80qior_FbI41Jgt7n8oSTVcH9Sufje0ob4yFt1JIpfw57Rv1mXw",
+  "expirationTime":null,
+  "keys":{
+     "p256dh":"BPfJt1uldLssNsrM7vfymzUHXBw_KI2I8GHt_oX23aUPiCkZ_5yO3R53Yzt78B1aOT2U5r7GUuEvPKkZrDIZJ6U",
+     "auth":"Pp2BPubL0IPWsOMliByfjA"
+  }
+};
 
 push.sendNotification(sub, 'test message');
